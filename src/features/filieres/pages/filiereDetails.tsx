bde57125fc4ff8635184fa97e8645da2ffb06473
EMILy2400 <maftahimane98@gmail.com>
2025-12-16 12:20:31 +0100

    Filiere initialisation

---
+++ src/features/filieres/pages/filiereDetails.tsx
@@ -0,0 +1,11 @@
+import FiliereDetails from "../components/filiereDetails";
+
+const FiliereDetailsPage = () => {
+  return (
+    <div>
+      <FiliereDetails />
+    </div>
+  );
+};
+
+export default FiliereDetailsPage;
